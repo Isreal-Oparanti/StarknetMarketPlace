dfcefbb405f255b17835d88512ed68aa9024f794
Isreal Oparanti <isrealopa@gmail.com>
2025-01-20 20:57:56 +1200

    fixing syntax err2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    });
+    return config;
+  },
 };
-
-module.exports = nextConfig;
